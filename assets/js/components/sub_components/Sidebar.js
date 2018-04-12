d325b28f39688a407175d416ca5e99a2a5856557
Andrew Grossfeld <amg445@cornell.edu>
2018-04-12 16:43:06 -0400

    more recommendation style changes

--- assets/js/components/sub_components/Sidebar.js
+++ assets/js/components/sub_components/Sidebar.js
@@ -20,21 +20,23 @@ class Sidebar extends React.Component {
   //Maps the insurance info into the final list that is displayed
   listContent = () => {
     return this.state.values.map((item) => (
-           <List.Item key={item.title}>
-           <Icon name='circle thin' className='sidebarIcon'/>
-           <div className={item.active == true ? 'activeText' : 'greyText'}>
+      <List.Item key={item.title}>
+        <div className="sidebar-item">
+          <div className={item.active == true ? 'activeText' : 'greyText'}>
+            <Icon name='circle thin' className='sidebarIcon'/>
             <p className='sidebarTextLeft'>{item.title}</p>
             <p className='sidebarTextRight'>${item.value}</p>
-            </div>
-           </List.Item>
-          ))
+          </div>
+        </div>
+      </List.Item>
+    ));
   }
 
   render() {
     console.log(this.listContent())
     return (
       <div id='sidebar' className='sidebar'>
-        <h1 id="sidebar-title">Jetson Recommends</h1>
+        <h1 id="sidebar-title">JETSON RECOMMENDS</h1>
       	<List>
           {this.listContent()}
         </List>
