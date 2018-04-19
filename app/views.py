75367cc64536a9d6a827e29d1e768e961c1ef446
Andrew Grossfeld <amg445@cornell.edu>
2018-04-19 14:39:05 -0400

    added baseURL to env for upcoming ajax calls

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from django.template.response import TemplateResponse
 def main(request):
     return TemplateResponse(request, 'templates/index.html', { 
         'env': {
+            'baseURL': request.META['HTTP_HOST'],
             'device': request.user_agent.device.family,
             'is_mobile': request.user_agent.is_mobile
         }
